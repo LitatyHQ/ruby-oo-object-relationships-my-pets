c259b94b2b06fa10cfa77be9f32b6cd8216a9cf6
LitatyHQ <litatyHQ@gmail.com>
2020-11-05 20:41:09 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,75 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}." 
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.size
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+# goes thru all the cats and adds the owner 
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+   #when we buy a cat, it adds to the cats we have 
+   #plus belongs to the owner
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
+  # when we walk all the dogs, they get happy
+  # we use all.select to "walk" each one & make them happy
+  def walk_dogs
+    Dog.all.select do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    Cat.all.select do |cat|
+      cat.mood = "happy"
+    end
+  end
+    #you're telling it what pets are and then writing the code it's asking for
+    #
+  def sell_pets
+    pets = self.dogs + self.cats
+
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
